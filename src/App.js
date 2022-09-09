fdb201ffe8c59b66eb2739b6341a671da970447c
nathan <ntflach@edu.uwaterloo.ca>
2022-09-09 20:53:00 +0200

    fixed issue with gh pages showing readme instead of main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Home from './components/Home';
+
+/*
+Notes
+- Need Home, About, Projects, Contact, Resume sections on nav
+- Inspo: cargo.site, dellsystem, Marc Tessyier, Bratton, dunne/raby, 
+- Hi! I'm Nathan. 
+
+*/
+
 
 function App() {
   return (
-    <div className="App"></div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,7 @@
+import './index.scss';
+
+const Home = () => {
+    return <>Hello HOME</>
+}
+
+export default Home;
