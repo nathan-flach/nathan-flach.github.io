1b72b6ea000d74d543729adc78d896948c6388c2
nathan <ntflach@edu.uwaterloo.ca>
2022-09-11 19:33:37 +0200

    removed todo list from app.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import Projects from "./components/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-/*
-Notes TODO
-- CLEANUP
-- Header and footer inline not stacked
-- White text on black over header photo
-- Header: left side HOME/NATHAN FLACH, (about?), PROJECTS, RESUME
-
-- CLEANUP
-*/
-
 function App() {
   return (
     <div className="App">
