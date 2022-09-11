708660ef6f39ee334c3b3e538cb1e0d92673c118
nathan <ntflach@edu.uwaterloo.ca>
2022-09-11 21:57:32 +0200

    add en dash for hci lol, also starting on proj section

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,12 @@ import './index.scss';
 const Projects = () => {
     return (
        <div className='projects'>
+            <h3>projects</h3>
+            
+            
+            <div className='project-list'>
 
+            </div>
        </div> 
     );
 }
