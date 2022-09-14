9f878207030b918cb18badea92a4570b06b973b2
nathan <ntflach@edu.uwaterloo.ca>
2022-09-14 23:38:51 +0200

    ok that worked but # was really ugly and resume didn't load. probably all hrefs need to get changed then? sigh -- just gonna revert back for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Router baseline="/">
+      <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
